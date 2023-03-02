f21a9d7c8ec5351310353af3a96fa9cd53142fae
przemjan <przemjanisz2@gmail.com>
2023-03-02 07:18:14 +0100

    commit nr 3

---
+++ src/Potega.java
@@ -0,0 +1,13 @@
+public class Potega {
+    public static void main(String[] args) {
+
+        double podstawa = 2.0;
+        double wykladnik = 3.0;
+
+        double wynik = Math.pow(podstawa, wykladnik);
+
+        System.out.println(podstawa + " do potęgi " + wykladnik +
+                " = " + wynik);
+    }
+
+}
